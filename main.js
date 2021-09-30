da26ccf16de33b82429e36643f056c6b2082adcd
Juhi <mahima061998@gmail.com>
2021-09-30 16:33:25 +0530

    HTML CSS and JS for the website

---
+++ main.js
@@ -0,0 +1,13 @@
+﻿const modalWrapper = document.querySelector(".modals-wrapper");
+
+function displayModal(id) {
+    const modal = document.getElementById(id);
+    modalWrapper.style.display = "flex";
+    modal.style.display = "flex";
+    //close-modal
+    const close = document.getElementById("close-modal");
+    close.addEventListener("click", () => {
+    modalWrapper.style.display = "none";
+    modal.style.display = "none";
+    })
+}
